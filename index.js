42333d60d86957fe4edddf1d4f162ec5bb3088c7
Rafał Kuchta <rafalmax12@interia.pl>
2019-08-16 20:07:16 +0200

    Add toucmove event

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const animBG = new AnimBg('.anim-bg');
 
     document.addEventListener('mousemove', (event) => animBG.listenCursorMove(event))
+    document.addEventListener('touchmove', (event) => animBG.listenCursorMove(event))
 
 
 
